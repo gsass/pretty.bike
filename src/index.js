50f6502c5ccc37d1554b3453c353b5236c61fb42
George Sass <george.a.sass@gmail.com>
2021-03-28 13:02:32 -0700

    Hook up markdown rendered

--- src/index.js
+++ src/index.js
@@ -1,9 +1,19 @@
-var app = Vue.createApp({
-	data() {
-	    return {
-	      message: 'Hey.'
-	    }
-  	},
-});
+const app = Vue.createApp({
+  data() {
+    return {
+      input: '# Hey.'
+    }
+  },
+  computed: {
+    rendered() {
+      return marked(this.input, { sanitize: true });
+    }
+  },
+  /** methods: {
+    update: _.debounce(function(e) {
+      this.input = e.target.value;
+    }, 300)
+  } */
+})
 
 app.mount("#app");
