d56eafb294080773e719ac4ee01ab093356d4a5f
George Sass <george.a.sass@gmail.com>
2021-03-29 14:16:04 -0700

    Add navigation bar component

--- src/index.js
+++ src/index.js
@@ -1,22 +1,27 @@
-const app = Vue.createApp({
-  data() {
-    return {
-      input: '# Hey.'
-    }
-  },
-  computed: {
-    rendered() {
-      return marked(this.input, { sanitizer:  DOMPurify.sanitize });
-    }
+/*
+* Define App components
+* TODO move this to a module when it hits 80+ ll.
+*/
+const navMenu = {
+  props: {
+    links: Array,
+    currentHash: String,
   },
   template: `
-    <div v-html="rendered"></div>
+    <ul>
+      <li v-for="link in links" :key="link.hash">
+        <a :href="link.hash">{{ link.label }}</a>
+      </li>
+    </ul>
   `
-})
+}
 
+/*
+* Set up the state manager, a `vuex` store
+*/
 const store = Vuex.createStore({
   state: {
-    path: '#'
+    path: window.location.hash
   },
   mutations: {
     setPath (state, newPath) {
@@ -25,11 +30,40 @@ const store = Vuex.createStore({
   },
   actions: {
     navigate ({ commit }, newPath) {
+      // TODO remove side effect from action!
       window.location.hash = newPath;
       commit('setPath', newPath);
     }
   }
 });
+
+
+/*
+* Set up the main app, and mount it in the container.
+*/
+const app = Vue.createApp({
+  data() {
+    return {
+      input: '# Hey.',
+      links: [
+        {hash: "#info", label: "Info"},
+        {hash: "#map", label: "Map"}
+      ],
+      currentHash: "#info"
+    }
+  },
+  computed: {
+    rendered() {
+      return marked(this.input, { sanitizer:  DOMPurify.sanitize });
+    }
+  },
+  template: `
+    <nav-menu :links="links"></nav-menu>
+    <div v-html="rendered"></div>
+  `
+})
+
+app.component('navMenu', navMenu)
 
 app.use(store);
 app.mount("#app");
