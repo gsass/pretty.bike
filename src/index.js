ae1f5d66ec81481eb77f2fe324ff2d0df3032cb0
George Sass <george.a.sass@gmail.com>
2021-03-30 15:40:15 -0400

    Make the state store actually navigate

--- src/index.js
+++ src/index.js
@@ -4,14 +4,14 @@
 */
 const navMenu = {
   props: {
-    links: Array,
+    routes: Array,
     currentHash: String,
   },
   template: `
     <div class="pure-menu">
       <ul class="pure-menu-list">
-        <li v-for="link in links" :key="link.hash" class="pure-menu-item">
-          <a :href="link.hash" class="pure-menu-link">{{ link.label }}</a>
+        <li v-for="route in routes" :key="route.hash" class="pure-menu-item">
+          <a :href="route.hash" class="pure-menu-link">{{ route.label }}</a>
         </li>
       </ul>
     </div>
@@ -21,9 +21,20 @@ const navMenu = {
 /*
 * Set up the state manager, a `vuex` store
 */
+
+// First, add a cache for fetched markdown content
+let contentCache = [
+  // Supports elements of form {hash: String, content: String},
+  {hash: '#', content: '# Hey.'},
+];
+
 const store = Vuex.createStore({
-  state: {
-    path: window.location.hash
+  state: { path: '#' },
+  getters: {
+    content: state => {
+      const cached = contentCache.find(item => item.hash === state.path) || {};
+      return cached.content;
+    }
   },
   mutations: {
     setPath (state, newPath) {
@@ -32,37 +43,40 @@ const store = Vuex.createStore({
   },
   actions: {
     navigate ({ commit }, newPath) {
-      // TODO remove side effect from action!
-      window.location.hash = newPath;
       commit('setPath', newPath);
     }
   }
 });
 
+// Navigate to the desired path before setting up the main app
+store.dispatch('navigate', window.location.hash || '#')
 
 /*
 * Set up the main app, and mount it in the container.
 */
+
+const routes = [ // Rotes for this app are all hash-based and all here
+];
+const notFoundMessage = '## 404! Sad Times, Dogg.';
+
 const app = Vue.createApp({
-  data() {
-    return {
-      input: '# Hey.',
-      links: [
-        {hash: "#info", label: "Info"},
-        {hash: "#map", label: "Map"}
-      ],
-      currentHash: "#info"
-    }
-  },
+  data() { return {
+    routes,
+    ...Vuex.mapState(['path',]),
+    ...Vuex.mapGetters(['content'])
+  }},
   computed: {
     rendered() {
-      return marked(this.input, { sanitizer:  DOMPurify.sanitize });
+      return marked(
+        this.content() || notFoundMessage,
+        { sanitizer:  DOMPurify.sanitize }
+      );
     }
   },
   template: `
     <div class="pure-g">
       <div id="menu" class="pure-u-1-5">
-        <nav-menu :links="links"></nav-menu>
+        <nav-menu :routes="routes"></nav-menu>
       </div>
       <div id="content" class="pure-u-4-5">
         <div v-html="rendered"></div>
