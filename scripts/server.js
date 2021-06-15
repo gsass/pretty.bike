cd3fee4cb0ee294f266627bbc1d5f392e0a10f9f
George Sass <george.a.sass@gmail.com>
2021-06-15 13:10:29 -0400

    Pull in most recent server script

--- scripts/server.js
+++ scripts/server.js
@@ -1,12 +1,15 @@
 const http = require('http');
 const fs = require('fs');
 const path = require('path');
+
+const baseDir = path.resolve(__dirname, '..')
 const PORT = 42069;
 
 const mimeTypes = {
   '.css': 'text/css',
   '.gif': 'image/gif',
   '.html': 'text/html',
+  '.ico': 'image/x-icon',
   '.jpg': 'image/jpg',
   '.js': 'text/javascript',
   '.json': 'application/json',
@@ -15,30 +18,28 @@ const mimeTypes = {
   '.svg': 'image/svg+xml'
 };
 
-http.createServer(function (request, response) {
+http.createServer((request, response) => {
   console.log(`Request: ${request.url}`);
 
-  const filePath = `../${request.url}`;
-  if (filePath == './') {
-    filePath = './index.html';
-  }
+  var filePath = request.url === '/' ? '/index.html' : request.url;
 
   var extname = String(path.extname(filePath)).toLowerCase();
 
   var contentType = mimeTypes[extname] || 'application/octet-stream';
 
-  fs.readFile(filePath, function(error, content) {
+  fs.readFile(baseDir + filePath, function(error, content) {
     if ( error )  {
       if ( error.code === 'ENOENT' ) {
+        console.log(`404 ${filePath}`);
         response.writeHead(404, { 'Content-Type': 'text/html' });
-        response.end(`Invalid path: ${filePath} ..\n`, 'utf-8');
-      }
-      else {
+        response.end(`Invalid file path: ${filePath}\n`, 'utf-8');
+      } else {
+        console.log(`500 ${filePath}`);
         response.writeHead(500);
-        response.end(`Unhandled error: ${error.code} ..\n`);
+        response.end(`Unhandled error: ${error.toString()}\n`, 'utf-8');
       }
-    }
-    else {
+    } else {
+      console.log(`200 ${filePath}`);
       response.writeHead(200, { 'Content-Type': contentType });
       response.end(content, 'utf-8');
     }
